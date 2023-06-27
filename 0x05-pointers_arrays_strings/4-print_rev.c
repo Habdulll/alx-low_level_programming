163fb34299a4ec6e6e9c63f09da6514a6ab61b97
Habdulll <abdullahgboyega16@gmail.com>
2023-06-26 20:11:17 -0700

    Task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,8 +6,8 @@
  */
 void print_rev(char *s)
 {
-int longi - 0;
-int 0;
+int longi = 0;
+int o;
 
 while (*s != '\0')
 {
@@ -15,11 +15,10 @@ longi++;
 s++;
 }
 s--;
-for (0 = longi; o > 0; o--)
+for (o = longi; o > 0; o--)
 {
 _putchar(*s);
 s--;
 }
 _putchar('\n');
 }
-

541c9242ddeb84d28a4551fc474dc2b0f24c8e1f
Habdulll <abdullahgboyega16@gmail.com>
2023-07-11 11:16:33 -0700

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,6 +1,6 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * str_concat - concatenates two strings of any size
@@ -9,6 +9,7 @@
  *
  * Return: the two strings concatenated
  */
+
 char *str_concat(char *s1, char *s2)
 {
 int i = 0, j = 0, k = 0, l = 0;

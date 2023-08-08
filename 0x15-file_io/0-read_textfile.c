45386fc451dc443463300d7f34b1c15ecccb9e78
Habdulll <abdullahgboyega16@gmail.com>
2023-08-07 17:11:58 -0700

    Task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,7 +10,7 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-char *car;
+char *buf;
 ssize_t fd;
 ssize_t w;
 ssize_t t;
@@ -18,8 +18,8 @@ fd = open(filename, O_RDONLY);
 if (fd == -1)
 return (0);
 buf = malloc(sizeof(char) * letters);
-t = read(fd, car, letters);
-w = write(STDOUT_FILENO, car, t);
+t = read(fd, buf, letters);
+w = write(STDOUT_FILENO, buf, t);
 free(buf);
 close(fd);
 return (w);
